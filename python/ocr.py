a9da03ba9e53d5c73d891210960f5a4441866d04
skatjfls <skatjfls1011@gnu.ac.kr>
2023-05-25 15:52:56 +0900

    남서린 ocr수정

--- python/ocr.py
+++ python/ocr.py
@@ -1,23 +1,5 @@
-# pip install tika # tika 라이브러리 설치
-<<<<<<< HEAD
-
 from tika import parser # pdf를 txt파일로 변환
 pdf_path = "C:\mysql_data\skatjfls.pdf"
-=======
-import pymysql
-from tika import parser # pdf를 txt파일로 변환
-
-conn = pymysql.connect(
-    host='localhost',
-    port=3306,
-    user='root',
-    password='123456',
-    db='everydata',
-    charset='utf8mb4'
-)
-
-pdf_path = "report_server.jsp.pdf"
->>>>>>> aa4d38d8bd05f78394f0ebb73de87261ec1ba657
 parsed = parser.from_file(pdf_path)
 txt = open('output.txt', 'w', encoding = 'utf-8')
 print(parsed['content'], file = txt)
@@ -31,6 +13,7 @@ def read_txt(filename, sep='', encoding='utf-8'):
             str[i] = str[i].strip().split(sep)
     return str
 
+    
     file.close()
 
 l1 = read_txt("output.txt", "\n") # 리스트를 띄어쓰기별로 구분
